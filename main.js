39cdf6a452b32aeb7369fe9cd8db3430a546e7cc
Patogallininin096 <113736189+Patogallininin096@users.noreply.github.com>
2023-02-20 17:40:24 -0600

    html.3

--- main.js
+++ main.js
@@ -3,10 +3,28 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  reconocimiento = ml5.imageClassifier("MobileNet", modeloListo)
 }
 function draw(){
-  Image(video, 0, 0, 300, 300);
+  image(video, 0, 0, 300, 300);
+  reconocimiento.classify(video, resultadoObtenido)
+}
+function modeloLiasto(){
+  console.log("modelo listo");
+}
+function resultadoObtenido(){
+  if(!error && resultado[0].confidence > 0.5 && resultadoAnterior != resultado[0].label){
+    document.getElementById("objeto").innerHTML =  resultado[0].label;
+    document.getElementById("confianza").innerHTML =  resultado[0].confidence;
+    hablar(resultado[0].label)
+    resultadoAnterior = resultado[0].label;
+  }
 }
 
-
+function hablar(mensaje){
+  var leerEnVozAlta = window.speechSynthesis;
+  var lectura = new SpeechSynthesisUtterance(mensaje);
+  lectura.lang = "en-US";
+  leerEnVozAlta.speak(lectura);  
+}
 
